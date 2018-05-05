4f377106b126b19794a8378c406459be1ff13daf
DownToSky <mme94@scarletmail.rutgers.edu>
2018-05-04 23:25:52 -0400

    extension option

--- background.js
+++ background.js
@@ -41,7 +41,7 @@ chrome.browserAction.onClicked.addListener(function(tab){
 });
 
 
- 
+
 //Returns the current local time in string format HH:MM:SS am|pm
 function currTime(){
 	var d = new Date();
@@ -84,3 +84,36 @@ chrome.alarms.onAlarm.addListener(function(){
 		console.log(Date.now());
         chrome.notifications.create(NotificationOptions);
     });
+
+    // Saves options to chrome.storage
+    function save_options() {
+      var color = document.getElementById('color').value;
+      var likesColor = document.getElementById('like').checked;
+      chrome.storage.sync.set({
+        favoriteColor: color,
+        likesColor: likesColor
+      }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+          status.textContent = '';
+        }, 750);
+      });
+    }
+
+    // Restores select box and checkbox state using the preferences
+    // stored in chrome.storage.
+    function restore_options() {
+      // Use default value color = 'red' and likesColor = true.
+      chrome.storage.sync.get({
+        favoriteColor: 'red',
+        likesColor: true
+      }, function(items) {
+        document.getElementById('color').value = items.favoriteColor;
+        document.getElementById('like').checked = items.likesColor;
+      });
+    }
+    document.addEventListener('DOMContentLoaded', restore_options);
+    document.getElementById('save').addEventListener('click',
+        save_options);
